dfdbb3c5a5c17fb77473446476664f731c452df1
Massimiliano <Massimiliano@Massimiliano.lan>
2018-05-30 21:15:37 +0200

    Realizzazione di controller, main e creazione model

--- Lab11_Bar/src/it/polito/tdp/bar/BarController.java
+++ Lab11_Bar/src/it/polito/tdp/bar/BarController.java
@@ -1,5 +1,47 @@
+/**
+ * Sample Skeleton for 'Bar.fxml' Controller Class
+ */
+
 package it.polito.tdp.bar;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.bar.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class BarController {
-	
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnSimula"
+    private Button btnSimula; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtArea"
+    private TextArea txtArea; // Value injected by FXMLLoader
+    
+    private Model model ;
+
+    @FXML
+    void handleSimula(ActionEvent event) {
+    	
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'Bar.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'Bar.fxml'.";
+
+    }
+
+	public void setModel(Model m) {
+		this.model = m ;
+	}
 }
